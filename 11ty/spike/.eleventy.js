81cc31a58ec3f7b3855e9ef82ae1667f4e148134
aleenaloves <aleena@bocoup.com>
2022-10-04 18:19:17 -0500

    added tailwindcss to 11ty

--- 11ty/spike/.eleventy.js
+++ 11ty/spike/.eleventy.js
@@ -1,13 +1,15 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/styles/tailwind.css")
     return {
         dir: {
-            input: 'src',
-            includes: '_includes',
-            output: '_site',
+          input: "src",
+          data: "_data",
+          includes: "_includes",
+          layouts: "_layouts"
         },
         templateFormats: ['md', 'njk', 'html'],
         markdownTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
-    };
+      };
 }
