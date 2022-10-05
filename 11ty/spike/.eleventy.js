b04fcd01cb1cb753868ef8b67549a8cd35075bf2
aleenaloves <aleena@bocoup.com>
2022-10-04 21:20:34 -0500

    bbuggy on styling with users

--- 11ty/spike/.eleventy.js
+++ 11ty/spike/.eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/styles/tailwind.css")
+    eleventyConfig.addPassthroughCopy("src/styles/")
     return {
         dir: {
           input: "src",
