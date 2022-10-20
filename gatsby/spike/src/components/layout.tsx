f7fa7f2910418e558fa81e682af32a445014f413
aleenaloves <aleena@bocoup.com>
2022-10-20 13:11:59 -0500

    adding in gatsby

---
+++ gatsby/spike/src/components/layout.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import Header from "./header"
+import Footer from "./footer"
+import Head from "./head"
+import "../styles.css"
+
+interface LayoutProps {
+  children: any
+  title: string
+  description?: string
+  image?: { id: string; url: string }
+}
+
+const Layout: React.FC<LayoutProps> = (props) => {
+  return (
+    <>
+      <Head {...props} />
+      <Header />
+      {props.children}
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
